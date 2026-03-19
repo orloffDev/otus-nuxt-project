176c4511ed3fb72031c98bae7eb26e6fb385d022
Sergey <sergey@kinoafisha.info>
2026-03-19 11:51:28 +0300

    add dynamic page user/[id]

---
+++ app/pages/user/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import {useRouter} from "nuxt/dist/app";
+  const route = useRoute();
+</script>
+
+<template>
+  <div>
+    user {{route.params.id}}
+  </div>
+</template>
